a0325b01d92d3be9c26d5be212c93379a1194ef1
QuevemCosta <quevemcostadacruz@gmail.com>
2020-08-01 21:28:33 -0300

    Pagina de produtos finalizada

---
+++ api/ofertas-home/ofertas.js
@@ -0,0 +1,45 @@
+const apiUrl = 'http://localhost:3333/products?';
+const page = 'page='+1;
+
+function listOfertas(){
+    fetch(apiUrl+page)
+    .then(response => response.json())
+    .then(data=>{
+        const products = data.data
+        products.map(function(product){
+            const {
+                cod,
+                img,
+                name,
+                parcelas,
+                status,
+                valorAprazo,
+				valorAvista
+
+            } = product
+
+            const ProductEle = document.createElement('li');
+			const ImageEle = document.createElement('img');
+            const NameEle = document.createElement('strong');
+            const ValueEle = document.createElement('span');
+		 
+        
+            const NameText = document.createTextNode(name);
+            const textValue = document.createTextNode(`A vista ${valorAvista} ou em até ${parcelas} de ${valorAprazo}`);
+     
+            ProductEle.setAttribute('id',cod);
+            ImageEle.setAttribute('src',img);
+            ValueEle.appendChild(textValue)
+            NameEle.appendChild(NameText)
+
+            ProductEle.appendChild(ImageEle);
+			ProductEle.appendChild(NameEle);
+            ProductEle.appendChild(ValueEle);
+            
+            document.querySelector('#ofertas').appendChild(ProductEle)
+
+        })
+    })
+} 
+
+listOfertas()
--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-function paginaCompraCamisa(url){
-    location.replace(url)
-}
-function chama_carro1(){
-    document.getElementById('carro-1').style.display="flex"
-    document.getElementById('carro-2').style.display="none"
-    document.getElementById('carro-3').style.display="none"
-    document.getElementById('carro-4').style.display="none"
-}
-function chama_carro2(){
-    document.getElementById('carro-1').style.display="none"
-    document.getElementById('carro-2').style.display="flex"
-    document.getElementById('carro-3').style.display="none"
-    document.getElementById('carro-4').style.display="none"
-}
-
-function chama_carro3(){
-    document.getElementById('carro-1').style.display="none"
-    document.getElementById('carro-2').style.display="none"
-    document.getElementById('carro-3').style.display="flex"
-    document.getElementById('carro-4').style.display="none"
-}
-    function chama_carro4(){
-    document.getElementById('carro-1').style.display="none"
-    document.getElementById('carro-2').style.display="none"
-    document.getElementById('carro-3').style.display="none"
-    document.getElementById('carro-4').style.display="flex"
-}
-
 //-----------------
 //Menu moble
 function abreMenu(){
