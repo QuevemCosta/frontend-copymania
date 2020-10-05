3e3b3ad253cda65e120b966c42aaaaa71024411a
Quevem Costa <quevemcostadacruz@gmail.com>
2020-10-04 23:05:27 -0300

    removendo espaços em brnco

--- src/api/products/click-product.js
+++ src/api/products/click-product.js
@@ -1,7 +1,8 @@
 const  listProduct = document.querySelectorAll('.ofertas li')
 for (const produto of listProduct) {
     produto.addEventListener('click',function(){
-        window.open(`https://api.whatsapp.com/send?phone=5593991234251&text=${produto.textContent}`)
+        const product = produto.textContent.trim()
+        window.open(`https://api.whatsapp.com/send?phone=5593991234251&text=${product}`)
      }
     )
 }
